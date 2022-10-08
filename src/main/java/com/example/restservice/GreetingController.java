16f425c568701430d9f572671084cbb5a70a9c01
segovelo <gauchosinpingo@gmail.com>
2022-10-08 15:21:24 +0100

    Changes to include Logging with .xml file configuration

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -2,18 +2,23 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class GreetingController {
-
+	private Logger logger = LoggerFactory.getLogger(GreetingController.class);
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/greeting")
+	@RequestMapping("/greetings")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		logger.debug("Inside greeting() method.");
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,13 +1,28 @@
 package com.example.restservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(RestServiceApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
+		try {
+			ApplicationContext ctx = SpringApplication.run(RestServiceApplication.class, args);
+		    Environment environment = ctx.getEnvironment();
+	        logger.info("Greetings Application started successfully !");
+	        logger.info(
+	          "Application listening on port: {}", environment.getProperty("local.server.port"));
+	    } catch (Exception ex) {
+	      logger.error("Greetings Application failed :( with exception ---> ", ex);
+	      throw new RuntimeException();
+	    }
+
 	}
 
 }
